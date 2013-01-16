7ba6789d4fae70be763b903721dc4582395b3ce1
Chris Barcroft <ChrisABarcroft@gmail.com>
2013-01-15 20:39:47 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sentrino::Application.config.secret_token = '1ea3f4aded477903e13f44e88f9ec91bc0fd1b469c2d9841e69604cc6bbed5a7618c2076cff596f64fd2c81465cf35092914d4d5c7b902a5c77ea3b47d2b8830'
