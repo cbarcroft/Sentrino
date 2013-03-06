851ca07476d98a23fbeca248a08aec58a78b4010
Chris Barcroft <ChrisABarcroft@gmail.com>
2013-03-05 17:05:23 -0800

    First pass at cron receiver

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
 	before_filter :find_device
 	before_filter :find_task, :only => [:show, :edit, :update, :destroy]
-	before_filter :find_actions_with_types, :only => [:new, :edit]
+	before_filter :find_actions_with_types, :only => [:new, :create, :edit, :update]
 
 	def new
 		@task = @device.tasks.build
@@ -40,6 +40,22 @@ class TasksController < ApplicationController
 		redirect_to @device
 	end
 
+	def cron
+		@tasks = Task.all
+		@active_crons = get_active_crons()
+
+		@tasks.each do |task|
+			if @active_crons[task.frequency]
+				result = task.device.send(task.action_type)
+				if task.result_action == "email"
+					#task.device.user.email
+				elsif task.result_action == "store"
+					#store as a result object (need to create)
+				end
+			end
+		end
+	end
+
 	private
 		def find_device
 			@device = Device.find(params[:device_id])
@@ -51,5 +67,19 @@ class TasksController < ApplicationController
 
 		def find_actions_with_types
 			@actions_with_types = @device.actions.joins(:action_type)
+		end
+
+		def get_active_crons
+			@current_hour = Time.now.strftime("%H").to_i
+			@current_minute = Time.now.strftime("%M").to_i
+
+			return {
+				"1M" => true,
+				"5M" => @current_minute%5 == 0 ? true : false,
+				"30M" => @current_minute%30 == 0 ? true : false.
+				"1H" => @current_minute == 0 ? true : false,
+				"12H" => @current_hour%12 == 0 && @current_minute == 0 ? true : false,
+				"24H" => @current_hour == 0 && @current_minute == 0 ? true : false
+			}
 		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130218222241) do
+ActiveRecord::Schema.define(:version => 20130305192000) do
 
   create_table "action_types", :force => true do |t|
     t.string   "name"
@@ -48,9 +48,10 @@ ActiveRecord::Schema.define(:version => 20130218222241) do
     t.string   "name"
     t.string   "frequency"
     t.integer  "action_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
     t.integer  "device_id"
+    t.string   "result_action"
   end
 
   add_index "tasks", ["action_id"], :name => "index_tasks_on_action_id"
