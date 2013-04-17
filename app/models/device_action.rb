cb3d0de2e7c4123304318cd0f3138775cc77d86f
Chris Barcroft <ChrisABarcroft@gmail.com>
2013-04-17 16:42:24 -0700

    Cleaned up codebase: Versioned all gems, removed the accidental 'json' folder gem installation, deleted obsolete models, used quiet-assets gem instead of initializer

--- app/models/device_action.rb
+++
@@ -1,5 +0,0 @@
-class DeviceAction < ActiveRecord::Base
-  attr_accessible :nickname
-  belongs_to :device
-  has_one :actiontype 
-end
--- app/models/tasktype.rb
+++
@@ -1,4 +0,0 @@
-class TaskType < ActiveRecord::Base
-  attr_accessible :name, :route
-  belongs_to :task
-end
